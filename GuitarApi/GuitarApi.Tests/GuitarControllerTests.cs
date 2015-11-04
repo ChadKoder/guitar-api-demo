45115e3ae85b7f4bde7333ad05928654217cbb99
Chad Keibler <chad.keibler@aesynt.com>
2015-11-04 10:45:10 -0500

    added unit tests for GuitarController, testing httpresponse

--- GuitarApi/GuitarApi.Tests/GuitarControllerTests.cs
+++ GuitarApi/GuitarApi.Tests/GuitarControllerTests.cs
@@ -1,8 +1,11 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Runtime.InteropServices;
+using System.Web.Http;
+using System.Web.Http.Hosting;
 using GuitarApi.controllers;
+using GuitarApi.Interfaces;
 using NUnit.Framework;
 using Rhino.Mocks;
 
@@ -11,11 +14,32 @@ namespace GuitarApi.Tests
     [TestFixture]
     public class GuitarControllerTests
     {
-        [Test]
-        public void Test_unit_tests_run()
+        private IGetGuitarsByCompany _getGuitarsByCompany;
+        private GuitarController _ctrl;
+
+        [SetUp]
+        public void Setup()
         {
-            var test = 1 + 1;
-            Assert.AreEqual(2, test);
+            _getGuitarsByCompany = MockRepository.GenerateMock<IGetGuitarsByCompany>();
+            _ctrl = new GuitarController(_getGuitarsByCompany);
+            _ctrl.Request = new HttpRequestMessage();
+            //_ctrl.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, (object)new HttpConfiguration());
+        }
+
+        [Test]
+        public void Get_with_search_text_returns_200OK_on_success()
+        {
+            _getGuitarsByCompany.Stub(x => x.Select(Arg<string>.Is.Anything)).Return(new List<Guitar>());
+            var result = _ctrl.Get("text");
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [Test]
+        public void Get_with_search_returns_500_on_exception()
+        {
+            _getGuitarsByCompany.Stub(x => x.Select(Arg<string>.Is.Anything)).Throw(new Exception());
+            var result = _ctrl.Get("text");
+            Assert.AreEqual(HttpStatusCode.InternalServerError, result.StatusCode);
         }
     }
 }
