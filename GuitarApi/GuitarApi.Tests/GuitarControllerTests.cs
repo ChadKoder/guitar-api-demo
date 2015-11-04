d7e24fbfaaac925bfbcc55ff17bac119b0b2a8fe
Chad Keibler <chad.keibler@aesynt.com>
2015-11-03 20:14:38 -0500

    added unit test project

---
+++ GuitarApi/GuitarApi.Tests/GuitarControllerTests.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using GuitarApi.controllers;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace GuitarApi.Tests
+{
+    [TestFixture]
+    public class GuitarControllerTests
+    {
+        [Test]
+        public void Test_unit_tests_run()
+        {
+            var test = 1 + 1;
+            Assert.AreEqual(2, test);
+        }
+    }
+}
